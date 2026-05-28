74544363f34533ddd27bc1fa2aa6ab6892f0ab8c
omsare6969 <erasmonegocio@gmail.com>
2026-05-27 21:08:39 -0300

    Correcao Chrome GitHub Actions

--- web_tests/utils/driver.py
+++ web_tests/utils/driver.py
@@ -1,16 +1,25 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
 
 def iniciar_driver():
+
+    chrome_options = Options()
+
+    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--window-size=1920,1080")
 
     service = Service(
         ChromeDriverManager().install()
     )
 
-    driver = webdriver.Chrome(service=service)
-
-    driver.maximize_window()
+    driver = webdriver.Chrome(
+        service=service,
+        options=chrome_options
+    )
 
     return driver
